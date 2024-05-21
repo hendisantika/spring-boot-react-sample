93fcb9a6408303bf67a9b49cacfa7b42c6a7de7b
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:33:44 +0700

    Add OrderService🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/service/OrderService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.orderservice.service;
+
+import id.my.hendisantika.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/22/24
+ * Time: 06:33
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+}
