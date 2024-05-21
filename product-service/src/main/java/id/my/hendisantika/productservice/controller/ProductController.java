f86bc7a97a7777ac091ef37c7f5d1f80f555b5d7
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:29:03 +0700

    Add getAllProducts in ProductController🫸🌀✏️📗🐧🐳

--- product-service/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
+++ product-service/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.productservice.controller;
 
+import id.my.hendisantika.productservice.entity.Product;
 import id.my.hendisantika.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
