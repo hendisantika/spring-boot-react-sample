24cbe196461de8966144d9a839c8b93051c00934
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:40:43 +0700

    Add corsWebFilter in ApiGatewayConfiguration🫸🌀✏️📗🐧🐳

--- api-gateway/src/main/java/id/my/hendisantika/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/id/my/hendisantika/apigateway/ApiGatewayConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,18 @@ public class ApiGatewayConfiguration {
                 .route("order_service", r -> r.path("/orders/**")
                         .uri("lb://order-service"))
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // You can specify the allowed origins here
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(corsConfigurationSource);
     }
 }
