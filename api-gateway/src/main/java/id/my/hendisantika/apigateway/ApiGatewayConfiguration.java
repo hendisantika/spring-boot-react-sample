4f636ca6560f13ad8b9b314cfdacb5d4ddd23798
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:39:55 +0700

    Add ApiGatewayConfiguration🫸🌀✏️📗🐧🐳

---
+++ api-gateway/src/main/java/id/my/hendisantika/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/22/24
+ * Time: 06:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product_service", r -> r.path("/products/**")
+                        .uri("lb://product-service"))
+                .route("order_service", r -> r.path("/orders/**")
+                        .uri("lb://order-service"))
+                .build();
+    }
+}
