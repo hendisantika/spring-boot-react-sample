4fe533e02fc9460939cbeef4a9ebd88121faf074
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:17:39 +0700

    Add Product Service Module🫸🌀✏️📗🐧🐳

---
+++ product-service/src/test/java/id/my/hendisantika/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
