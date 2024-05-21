b663a4e43ad3b2368151fe347c6923474d0622c7
hendisantika <hendisantika@yahoo.co.id>
2024-05-22 06:24:57 +0700

    Add Product Entity🫸🌀✏️📗🐧🐳

---
+++ product-service/src/main/java/id/my/hendisantika/productservice/entity/Product.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.productservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/22/24
+ * Time: 06:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+}
